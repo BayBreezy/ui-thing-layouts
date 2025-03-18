2951fa14350c587719f8e6d07c6449d529b5e83b
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-03-18 10:09:03 -0500

    chore(ai-chat): update tailwind config

--- ai-chat/tailwind.config.js
+++ ai-chat/tailwind.config.js
@@ -2,6 +2,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /**@type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./app/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
